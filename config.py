932eaab646b9ebfea9ae6d535d7563a37ea59171
creator154 <cteator898@gmail.com>
2026-03-17 21:22:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,8 +44,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/botscomplex")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+28QtimyLemZiZGVl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Shayar_Ki_Mahefil")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+I6SxNjjqpbgyZWU1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -89,12 +89,12 @@ autoclean = []
 confirmer = {}
 file_cache: dict[str, float] = {}
 
-START_IMG_URL = ["https://files.catbox.moe/78yc1t.jpg",
-                 "https://files.catbox.moe/78yc1t.jpg",
-                 "https://files.catbox.moe/78yc1t.jpg"]
+START_IMG_URL = ["https://files.catbox.moe/mw3vbo.jpg",
+                 "https://files.catbox.moe/mw3vbo.jpg",
+                 "https://files.catbox.moe/mw3vbo.jpg"]
     
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/87f680aead03443f291b0.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/mw3vbo.jpg"
 )
 PLAYLIST_IMG_URL = "https://graph.org/file/c95a687e777b55be1c792.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/edd388a42dd2c499fd868.jpg"
